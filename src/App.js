6731a4a8c36bcacf745e09871fa1870b6b0a03af
Cemre Aktemur <cemre_aktemur@hotmail.com>
2020-12-28 12:06:32 +0300

    Header added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Home.css"
+import Banner from './Banner'
+
+//rfce
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Homes</h1>
+            <Banner/>
+        </div>
+    )
+}
+
+export default Home
